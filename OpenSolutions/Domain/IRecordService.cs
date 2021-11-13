7ce2a02ea38eecfaa0fe05abedff58dae6539e0e
Vardan Karapetian <vardankaradas@gmail.com>
2021-11-13 14:08:35 +0300

    initial commit

---
+++ OpenSolutions/Domain/IRecordService.cs
@@ -0,0 +1,13 @@
+﻿using OpenSolutions.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenSolutions.Domain
+{
+    interface IRecordService
+    {
+        public List<RecordEntity> GetAll();
+    }
+}
